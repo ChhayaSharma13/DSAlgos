110d2e028ed1369256755a65cc93f0dd7cc3a538
chhaya <chhaya.sharma@ericsson.com>
2021-10-29 21:26:47 +0100

    Mathematical Formula for adding n numbers

--- Practice/Script/BigONotationScript.js
+++ Practice/Script/BigONotationScript.js
@@ -1,8 +1,15 @@
 function addNumbers(n){
     let sum = 0;
-    for(let i = 0; i < n; i++){
+    for(let i = 1; i <= n; i++){
         sum += i; 
     }
     return sum;
 }
-console.log(addNumbers(13));
+console.log(addNumbers(10));
+
+
+function addNumbersNumeric(n){
+    return (n * (n + 1)) / 2;
+}
+
+console.log(addNumbersNumeric(10));
