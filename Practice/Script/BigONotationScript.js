149f6ca52f023183c4e1aa1577a609c341a9ee95
chhaya <chhaya.sharma@ericsson.com>
2021-10-29 20:57:08 +0100

    Corrected Limit

--- Practice/Script/BigONotationScript.js
+++ Practice/Script/BigONotationScript.js
@@ -1,10 +1,8 @@
 function addNumbers(n){
     let sum = 0;
-    for(let i = 0; i <= n; i++){
+    for(let i = 0; i < n; i++){
         sum += i; 
     }
     return sum;
 }
 console.log(addNumbers(13));
-
-
